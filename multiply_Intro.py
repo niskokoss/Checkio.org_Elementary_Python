3ada26a1a6c236af7704d84d9522e0e98e3fd331
niskokoss <85651059+niskokoss@users.noreply.github.com>
2022-06-27 10:12:30 +0300

    Create multiply_Intro.py

---
+++ multiply_Intro.py
@@ -0,0 +1,14 @@
+# Напишите функцию, которая будет получать 2 числа и возвращать результат произведения этих чисел.
+# Входные данные: Два аргумента. Оба int
+# Выходные данные: Int.
+
+def mult_two(a: int, b: int) -> int:
+    return a * b
+
+print('Example')
+print(mult_two(1, 2))
+
+assert mult_two(3, 2) == 6
+assert mult_two(0, 1) == 0
+
+print("The first mission is done! Click 'Check' to earn cool rewards!")
