789aa7ee4edbea86738449ea7c265e00f0041723
niskokoss <85651059+niskokoss@users.noreply.github.com>
2022-07-09 12:02:21 +0300

    Create three_words.py

---
+++ three_words.py
@@ -0,0 +1,23 @@
+# Дана строка со словами и числами, разделенными пробелами (один пробел между словами и/или числами).
+# Слова состоят только из букв. Вам нужно проверить есть ли в исходной строке три слова подряд.
+# Для примера, в строке "start 5 one two three 7 end" есть три слова подряд.
+
+def checkio(words: str) -> bool:
+    t = 0
+    for i in words.split():
+        t = 0 if i.isnumeric() else t+1
+        if t == 3:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio("Hello World hello"))
+    
+    assert checkio("Hello World hello") == True, "Hello"
+    assert checkio("He is 123 man") == False, "123 man"
+    assert checkio("1 2 3 4") == False, "Digits"
+    assert checkio("bla bla bla bla") == True, "Bla Bla"
+    assert checkio("Hi") == False, "Hi"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
