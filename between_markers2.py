9aa36a391d9f01e8e1d9ce47fcc701893708a678
niskokoss <85651059+niskokoss@users.noreply.github.com>
2022-07-15 19:04:59 +0300

    Create between_markers2.py

---
+++ between_markers2.py
@@ -0,0 +1,34 @@
+# Вам дана строка и два маркера (начальный и конечный).
+# Вам необходимо найти текст, заключенный между двумя этими маркерами.
+# Но есть несколько важных условий:
+# 
+# Начальный и конечный маркеры всегда разные
+# Если нет начального маркера, то началом считать начало строки
+# Если нет конечного маркера, то концом считать конец строки
+# Если нет ни конечного, ни начального маркеров, то просто вернуть всю строку
+# Если конечный маркер стоит перед начальным, то вернуть пустую строку
+
+def between_markers(text: str, begin: str, end: str) -> str:
+    if begin not in text and end not in text:
+        return text
+    if begin not in text:
+        return text[:text.find(end)]
+    if end not in text:
+        return text[text.find(begin) + len(begin):]
+    x = text[text.find(begin) + len(begin):text.find(end)]
+    return x
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers("<head><title>My new site</title></head>",
+                           "<title>", "</title>") == "My new site", "HTML"
+    assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+    assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
+    assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+    assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
+    print('Wow, you are doing pretty good. Time to check it!')
