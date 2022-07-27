7863c4414098e35ec2a838ec83fdd1efcd40b6d8
niskokoss <85651059+niskokoss@users.noreply.github.com>
2022-07-27 17:50:12 +0300

    Create acceptable_password_III.py

---
+++ acceptable_password_III.py
@@ -0,0 +1,28 @@
+# В этой миссии вам нужно создать функцию проверки пароля.
+# 
+# Это условия проверки:
+# 
+# длина должна быть больше 6;
+# должен содержать хотя бы одну цифру, но не может состоять только из цифр.
+
+
+def is_acceptable_password(password: str):
+    x = 0
+    for i in password:
+        if i.isdigit():
+            x += 1
+    if password.isdigit():
+        return False
+    return True if len(password) >= 6 and x >= 1 else False
+
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("muchlonger") == False
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    assert is_acceptable_password("1234567") == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
