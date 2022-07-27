0e38696d690b61b55fa1547bc63cf7b0c89d2312
niskokoss <85651059+niskokoss@users.noreply.github.com>
2022-07-27 18:14:46 +0300

    Create acceptable_password_V.py

---
+++ acceptable_password_V.py
@@ -0,0 +1,39 @@
+# В этой миссии вам нужно создать функцию проверки пароля.
+#
+# Это условия проверки:
+#
+# длина должна быть больше 6;
+# должен содержать хотя бы одну цифру, но не может состоять только из цифр;
+# наличие цифр или просто цифр не применяется к паролю длиннее 9.
+# строка ни в коем случае не должна содержать слово «пароль».
+def is_acceptable_password(password: str):
+    x = 0
+    for i in password:
+        if i.isdigit():
+            x += 1
+    if "password" in password.lower():
+        return False
+    elif len(password) > 9:
+        return True
+    elif password.isdigit():
+        return False
+    elif len(password) >= 6 and x >= 1:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("short54") == True
+    assert is_acceptable_password("muchlonger") == True
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    assert is_acceptable_password("1234567") == False
+    assert is_acceptable_password("12345678910") == True
+    assert is_acceptable_password("password12345") == False
+    assert is_acceptable_password("PASSWORD12345") == False
+    assert is_acceptable_password("pass1234word") == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
